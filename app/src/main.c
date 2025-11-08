c850b0d1512e9a59f7ba89db7d7f22e479d37136
mtkaz1 <muhammadkazi144@gmail.com>
2025-11-07 18:31:42 -0700

    initialize LED pin

--- app/src/main.c
+++ app/src/main.c
@@ -8,8 +8,25 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-int main(void) {
 
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+
+
+
+int main(void) {
+  int ret;
+
+  if(!gpio_is_ready_dt(&led0)) {
+    return -1;
+  }
+
+  ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0) {
+    return ret;
+  }
   while(1) {
 
   }
