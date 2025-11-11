20d6b2a0e0afb4beea408a5ddb22eb9668f3d14c
mtkaz1 <muhammadkazi144@gmail.com>
2025-11-10 18:05:59 -0700

    LED4 blinking at double the speed

--- app/src/main.c
+++ app/src/main.c
@@ -61,17 +61,15 @@ int main(void) {
   }
 
 
-
-
-
-
   while(1) {
     gpio_pin_toggle_dt(&led0);
     gpio_pin_toggle_dt(&led1);
     gpio_pin_toggle_dt(&led2);
     gpio_pin_toggle_dt(&led3);
 
-    k_msleep(1000);
+    k_msleep(500);
+    gpio_pin_toggle_dt(&led3);
+    k_msleep(500);
   }
 	return 0;
 }
